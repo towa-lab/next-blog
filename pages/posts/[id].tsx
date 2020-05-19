a541d04e814b301d934385098348e734aa4496a2
Koji Ishimoto <koji.ishimoto@gmail.com>
2020-05-20 01:39:07 +0900

    add typescript

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -4,7 +4,15 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
 
-export default function Post({ postData }) {
+import { GetStaticProps, GetStaticPaths } from 'next'
+
+export default function Post({ postData }: {
+  postData: {
+    title: string
+    date: string
+    contentHtml: string
+  }
+}) {
   return (
     <Layout>
       <Head>
@@ -21,7 +29,7 @@ export default function Post({ postData }) {
   )
 }
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds()
   return {
     paths,
@@ -29,8 +37,8 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string)
   return {
     props: {
       postData
